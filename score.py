880d9746f4128abac01447a72cc9c41400786d60
HS-Anand <156099405+HS-Anand@users.noreply.github.com>
2024-08-11 18:07:10 +0530

    Update score.py

--- score.py
+++ score.py
@@ -12,17 +12,32 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
+        score_file = open("data.txt", mode="r")
+        self.high_score = int(score_file.read())
+        score_file.close()
         self.update_scoreboard()
 
-
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} || High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def game_over(self):
         self.goto(0, 0)
         self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
+    def pause(self):
+        self.clear()
+        self.write("GAME PAUSED", align=ALIGNMENT, font=FONT)
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        score = open("data.txt", mode="w")
+        high_score = str(self.high_score)
+        score.write(high_score)
+        score.close()
+        self.score = 0
+
